4f38125cc3e06a9f5bfbf0818f63503747db1e2d
elanor <elanor.m.moore@gmail.com>
2021-02-14 09:57:27 -0800

    Complete gallery page

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 function validate() {
     var username = document.getElementById('name-input').value;
+    console.log(username);
     if (username == "Victor" || username == "victor" || username == "VICTOR") {
         document.getElementById('login-page').classList.add('disabled');
+        document.getElementById('gallery-page').classList.add('enabled');
         return false;
     }
     else {
@@ -9,3 +11,6 @@ document.getElementById('warning').classList.add('enabled');
     }
     return false;
 }
+
+document.onkeyup = enter;    
+function enter(e) {if (e.which == 13) validate();}
